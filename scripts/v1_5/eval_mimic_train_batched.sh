a2756bbcd13342a48f8a3675f8be4255e5005d02
runhuixu <2021myxrh@sjtu.edu.cn>
2026-01-13 00:09:46 -0800

    add batch evaluation on training script

--- scripts/v1_5/eval_mimic_train_batched.sh
+++ scripts/v1_5/eval_mimic_train_batched.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=llava_mimic_eval_train_batched
 #SBATCH --partition=gpu
-#SBATCH --gres=gpu:a40:1
+#SBATCH --gres=gpu:l40s:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
